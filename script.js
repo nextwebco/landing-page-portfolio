9a55b8b8ace917166e2aed9a6583ef0c51890932
Luiz Henrique <luizhenriqueguguilusu@gmail.com>
2026-04-27 22:51:33 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const faqItems = document.querySelectorAll(".faq-item");
-
-faqItems.forEach((item) => {
-  const button = item.querySelector(".faq-question");
-
-  button.addEventListener("click", () => {
-    item.classList.toggle("active");
-  });
-});
