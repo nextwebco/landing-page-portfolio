beed04edf8aa2413218464bf76319485c2f1f776
Luiz Henrique <luizhenriqueguguilusu@gmail.com>
2026-04-27 22:52:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+  const button = item.querySelector(".faq-question");
+
+  button.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
